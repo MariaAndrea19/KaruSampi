f38faf1f14f4a70adcce926b6d269a399c3a838c
UTECEduardoStudent <eduardo.castro@utec.edu.pe>
2019-11-28 14:00:15 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/KaruSampi.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KaruSampi.dir/main.cpp.obj"
+  "KaruSampi.exe"
+  "KaruSampi.exe.manifest"
+  "KaruSampi.lib"
+  "KaruSampi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KaruSampi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
