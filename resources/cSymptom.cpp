ddf3061fd2dbc3df167f5b1f16442c5d2f649e22
UTECEduardoStudent <eduardo.castro@utec.edu.pe>
2019-11-28 14:58:17 -0500

    cDisease and cPatient added.

---
+++ resources/cSymptom.cpp
@@ -0,0 +1,5 @@
+//
+// Created by eduar on 28-Nov-19.
+//
+
+#include "cSymptom.h"
---
+++ resources/cSymptom.h
@@ -0,0 +1,19 @@
+//
+// Created by eduar on 28-Nov-19.
+//
+
+#ifndef KARUSAMPI_CSYMPTOM_H
+#define KARUSAMPI_CSYMPTOM_H
+
+
+#include "../types.h"
+
+class cSymptom {
+    text_t name;
+    state_t presence;
+public:
+    cSymptom(text_t &n, state_t &p): name(n), presence(p) {}
+};
+
+
+#endif //KARUSAMPI_CSYMPTOM_H
